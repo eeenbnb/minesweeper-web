45abb743b19293f4c9a1b41d26e4af03894d61f7
eeenbnb <eee.kz.www@gmail.com>
2021-06-07 21:35:08 +0900

    fix,functions.

--- src/logic/openMine.ts
+++ src/logic/openMine.ts
@@ -2,13 +2,19 @@ import { Mains } from '../@types/main'
 import { getAroundItems } from './getAroundItems'
 
 export const openMine = (mains: Mains, i: number, j: number): void => {
+  if(mains[i][j].isOpened){
+    return ;
+  }
   mains[i][j].isOpened = true;
+
+  if(mains[i][j].isBom){
+    alert("boom");
+    return ;
+  }
 
   if (mains[i][j].count == 0) {
     getAroundItems(mains,i,j).forEach((item)=>{
-      if(!mains[item.i][item.j].isOpened){
-        openMine(mains, item.i, item.j);
-      }
+      openMine(mains, item.i, item.j);
     })
   }
 }
